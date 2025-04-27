0560db5f93b2ca8df14c742dab4738af170626e5
Fernando Emanuel <78227397+femm34@users.noreply.github.com>
2025-04-27 16:25:28 -0600

    feat: auth templete finished

--- src/main/java/com/fervanz/auth/security/services/IRoleService.java
+++ src/main/java/com/fervanz/auth/security/services/IRoleService.java
@@ -1,7 +1,6 @@
 package com.fervanz.auth.security.services;
 
 import com.fervanz.auth.security.models.entities.CustomRole;
-import com.fervanz.auth.security.models.enums.RoleEnum;
 
 import java.util.Set;
 
